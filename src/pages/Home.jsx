7e554c2050ba3e55e195c596003f299d80da7835
sohibjonabdialimov <sohibjonabdialimov6@gmail.com>
2024-03-25 14:11:44 +0500

    change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 
 const Home = () => {
+  const {t} = useTranslation(["home", "common"])
   return (
     <div className='text-3xl font-bold underline text-red-800'>Home</div>
   )
