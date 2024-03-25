43995d0f6ef6321a83c9406edac20c82fd32677a
sohibjonabdialimov <sohibjonabdialimov6@gmail.com>
2024-03-25 13:10:27 +0500

    installing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Header from "./components/navbar/Header";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
     <>
-      <h1>Multi Languages App</h1>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/profile" element={<Profile />} />
+      </Routes>
     </>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='text-3xl font-bold underline text-red-300'>Home</div>
+  )
+}
+
+export default Home
